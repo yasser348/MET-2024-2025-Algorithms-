a5ceea44091442c5a34dd5b3c77f373112517720
a7med wagih <alalfya821@gmail.com>
2024-10-18 17:45:49 +0300

    Add files via upload

---
+++ ahmed wagih elalfy.py
@@ -0,0 +1,18 @@
+            #sec 5
+
+def selection_sort(arr):
+    for i in range(len(arr)):
+        min_idx = i
+        for j in range(i+1, len(arr)):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+       
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+        
+        print(f"Step {i+1}: {arr}")
+
+
+arr = [64, 25, 12, 22, 11]
+print(arr)
+selection_sort(arr)
+print(arr)
