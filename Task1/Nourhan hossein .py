786f0196873c7d599bbce09617e166c3f975ed29
Nada Rabea <115660705+Nada-Elsawah@users.noreply.github.com>
2024-10-18 23:48:52 +0300

    Create Nourhan hossein .py

---
+++ Task1/Nourhan hossein .py
@@ -0,0 +1,24 @@
+‎#نورهان حسين احمد فتوح 
+#section 1 
+def selection_sort(arr):
+    
+    n = len(arr)
+
+    for i in range(n):
+       
+        min_index = i
+        
+      
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+    
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+        
+       
+        print(f"After iteration {i + 1}: {arr}")
+
+numbers = [64, 25, 12, 22, 11]
+print("Original array:", numbers)
+selection_sort(numbers)
+print("Sorted array:", numbers)
